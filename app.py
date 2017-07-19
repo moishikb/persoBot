ecd8125e3d5a1cdc8eda66676075cd481e35c5e3
boim <boim.moshe@gmail.com>
2017-07-19 11:50:49 +0300

    redecorat the app file

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 import socket
 
 application = Flask(__name__)
+
+
 @application.route('/')
-def showMachineList():
+def show_machine_list():
     return render_template('list.html')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
 
-	
+
 @application.route("/execute",methods=['GET'])
 def execute():
     try:
-		return render_template('list2.html')
-		return socket.gethostbyname(socket.gethostname())
+        #return render_template('list2.html')
+        return socket.gethostbyname(socket.gethostname())
     except Exception, e:
         print 'Error is ' + str(e)
         return jsonify(status='ERROR',message=str(e))
