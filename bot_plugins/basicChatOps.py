b24b7dd576af765ec8a17df7689cfa3cafe4efcc
boim <boim.moshe@gmail.com>
2017-07-27 22:54:22 +0300

    1. adding template for package info methods

--- bot_plugins/basicChatOps.py
+++ bot_plugins/basicChatOps.py
@@ -13,3 +13,6 @@ def what_is_the_time():
 def task_manager():
     return os.popen(r"C:\Windows\System32\taskmgr.exe").read()
 
+def get_info():
+    info ='<div class="container"><div class="panel panel-primary"><div class="panel-heading">Info about <b>Basic Chat Ops </b> operations</div><div class="panel-body"><table class="table"><thead><tr><th>Function</th><th>Description</th><th>Notes</th></tr></thead><tbody><tr><td>say_hello</td><td>Just to say hello to the user </td><td>simple output</td></tr><tr><td>what_is_the_time</td><td>Return the current computer full time and date</td><td>time format</td></tr><tr><td>task_manager</td><td>Open the task manager</td><td></td></tr></tbody></table></div></div></div>'
+    return info
--- bot_plugins/os_extend.py
+++ bot_plugins/os_extend.py
@@ -6,3 +6,8 @@ def execute_dir():
 
 def execute__local_dir():
     return os.popen("dir").read()
+
+
+def get_info2():
+    info ='<div class="container"><div class="panel panel-default"><div class="panel-heading">Info about <b>os extend </b> operations</div><div class="panel-body"><table class="table"><thead><tr><th>Function</th><th>Description</th><th>Notes</th></tr></thead><tbody><tr><td>execute__local_dir</td><td>print dir results</td><td>simple output</td></tr><tr><td>execute_dir</td><td></td><td></td></tr></tbody></table></div></div></div>'
+    return info
